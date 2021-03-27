578bd3d63e46e0e1641de7218e8c3634c8484218
Albert Alonso <albert15_89@hotmail.com>
2021-03-27 15:52:16 +0100

    Pathfinding con Points, cambiarlo a Vector2

---
+++ NeoTile/Physics/Path/Node.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using NeoTile.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoTile.Physics.Path
+{
+    public class Node
+    {
+        public Point Position { get; set; }
+
+        public Node Parent { get; set; }
+
+        public int F { get; set; }
+        public int G { get; set; }
+        public int H { get; set; }
+
+        public Node(Point position)
+        {
+            Position = position;
+        }
+
+        public List<Node> NeighbourNodes(Tile[,] map)
+        {
+            List<Node> neighbourNodes = new List<Node>();
+
+            for (int y = 0; y < 2; y++)
+                for (int x = 0; x < 2; x++)
+                    if (!(x == 0 && y == 0) && !map[Position.X, Position.Y].IsBlock)
+                        neighbourNodes.Add(new Node(new Point(Position.X + x, Position.Y + y)));
+
+            return neighbourNodes;
+        }
+
+        public void CalculateFGH(Node startNode, Node endNode)
+        {
+            G = Math.Abs(Position.X - startNode.Position.X) + Math.Abs(Position.Y - startNode.Position.Y);
+
+            H = Math.Abs(Position.X - endNode.Position.X) + Math.Abs(Position.Y - endNode.Position.Y);
+
+            F = G + H;
+        }
+    }
+}
---
+++ NeoTile/Physics/Path/PathFinding.cs
@@ -0,0 +1,65 @@
+﻿
+using Microsoft.Xna.Framework;
+using NeoTile.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeoTile.Physics.Path
+{
+    public class PathFinding
+    {
+        private Node startNode, endNode, currentNode;
+        private List<Node> openNodes = new List<Node>();
+        private List<Node> closedNodes = new List<Node>();
+
+        public List<Point> SearchPath(Point startPosition, Point endPosition, Tile[,] map)
+        {
+            startNode   = new Node(startPosition);
+            endNode     = new Node(endPosition);
+
+            openNodes.Add(startNode);
+
+            while(openNodes.Count > 0)
+            {
+                currentNode = openNodes.OrderBy(node => node.F).First();
+
+                openNodes.Remove(currentNode);
+                closedNodes.Add(currentNode);
+
+                if (currentNode.Position == endPosition)
+                    break;
+
+                foreach (Node neighbourdNode in currentNode.NeighbourNodes(map))
+                {
+                    if (closedNodes.FirstOrDefault(node => node.Position == neighbourdNode.Position) != null)
+                        continue;
+
+                    if (openNodes.FirstOrDefault(node => node.Position == neighbourdNode.Position) == null)
+                    {
+                        openNodes.Add(neighbourdNode);
+                        neighbourdNode.Parent = currentNode;
+                        neighbourdNode.CalculateFGH(startNode, endNode);
+                    }
+                    else
+                    {
+                        if (neighbourdNode.G < currentNode.G)
+                        {
+                            neighbourdNode.Parent = currentNode;
+                            neighbourdNode.CalculateFGH(startNode, endNode);
+                        }
+                    }
+                }
+            }
+
+            List<Point> finalPositions = new List<Point>();
+            while(currentNode != null)
+            {
+                finalPositions.Add(currentNode.Position);
+                currentNode = currentNode.Parent;
+            }
+
+            return finalPositions;
+        }
+    }
+}
