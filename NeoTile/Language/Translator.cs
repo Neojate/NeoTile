136c1bdffa8ecd98d6991724bc9bf118b6d60e20
Albert Alonso <albert15_89@hotmail.com>
2021-04-02 23:44:33 +0200

    Translator

---
+++ NeoTile/Language/Translator.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NeoTile.Language
+{
+    public class Translator
+    {
+        private static readonly Lazy<Translator> Lazy = new Lazy<Translator>(() => new Translator());
+
+        public static Translator Instance { get { return Lazy.Value; } }
+
+        private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        public void LoadJson(string fileName)
+        {
+            string jsonString = File.ReadAllText(fileName);
+            dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+        }
+
+        public string Translate(string code)
+        {
+            try
+            {
+                return dictionary[code];
+            }
+            catch (Exception e)
+            {
+                return code;
+            }
+        }
+    }
+}
