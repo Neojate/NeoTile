a9c49ed669d3a5c923dddd95accb2dffc421187c
Albert Alonso <albert15_89@hotmail.com>
2021-11-21 11:37:46 +0100

    Componentes

---
+++ NeoTile/Components/Bar.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NeoTile.Input;
+using System;
+
+namespace NeoTile.Components
+{
+    public class Bar : Component
+    {
+        private Vector2 limit;
+
+        private WrapIndexer wrapIndexer;
+
+        private Vector2 barInitialPosition;
+
+        private Vector2 clickPosition;
+
+        private bool canMove = false;
+
+        public Bar(Rectangle bounds, Style style, Vector2 limit, WrapIndexer wrapIndexer)
+        {
+            Bounds = bounds;
+            Style = style;
+            this.limit = limit;
+            this.wrapIndexer = wrapIndexer;
+            
+            barInitialPosition = new Vector2(bounds.X, bounds.Y);
+        }
+
+        public override void HandleInput()
+        {
+            if (mouse.MouseDown() && Bounds.Contains(mouse.Position))
+            {
+                canMove = true;
+                clickPosition = Vector2.Subtract(mouse.Position, new Vector2(Bounds.X, Bounds.Y));
+            }
+
+            if (canMove && !mouse.MouseDown())
+                canMove = false;
+
+            if (canMove)
+            {
+                int deltaY = (int)(mouse.Position.Y - clickPosition.Y);
+                if (deltaY >= limit.X && deltaY + Bounds.Height <= limit.Y)
+                {
+                    wrapIndexer.Indexer = (deltaY - (int)barInitialPosition.Y) * -1;
+                    Bounds.Y = deltaY;
+                }
+            }
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Style.Texture, Bounds, Style.BgColor);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
--- NeoTile/Components/Overflow.cs
+++ NeoTile/Components/Overflow.cs
@@ -5,33 +5,144 @@ using System.Collections.Generic;
 
 namespace NeoTile.Components
 {
-    class Overflow : Component
+    public enum OverflowDirection
     {
-        public List<Div> Divs;
+        Bottom, Top
+    }
+
+    public class Overflow : Component
+    {
+        private List<Div> divs;
 
         private List<Component> components;
 
+        private int areaSize = 0;
+
+        private OverflowDirection direction;
+
+        private int indexer;
+
+        private WrapIndexer wrapIndexer;
+
+        private int startDivY;
+
+        private Style styleButton;
+
+        public int moveSpeed = 5;
+
+        private Bar bar;
+
+        public Overflow(List<Div> divs, Rectangle bounds, OverflowDirection direction, int wrap, Style styleButton)
+        {
+            this.divs = divs;
+            Bounds = bounds;
+            this.direction = direction;
+
+            if (divs.Count > 0)
+                areaSize = divs[0].Bounds.Height + wrap;
+
+            wrapIndexer = new WrapIndexer() { Indexer = indexer };
+            createInterface();
+        }
+
         public override void HandleInput()
         {
-            throw new NotImplementedException();
+            divs.ForEach(div => div.HandleInput());
+
+            components.ForEach(component => component.HandleInput());
         }
 
         public override void Render(SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            int y = direction == OverflowDirection.Top
+                ? Bounds.Y + wrapIndexer.Indexer
+                : Bounds.Y + wrapIndexer.Indexer + Bounds.Height - (divs.Count * areaSize);
+
+            startDivY = y;
+
+            divs.ForEach(div =>
+            {
+                if (y >= Bounds.Y && y <= Bounds.Y + Bounds.Height - areaSize)
+                {
+                    div.Bounds.Y = y;
+                    div.Render(spriteBatch);
+                    if (div.Children != null)
+                        div.Children.ForEach(child => child.Bounds.Y = y);
+                }
+                y += areaSize;
+            });
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            divs.ForEach(div => div.Update(gameTime));
+
+            components.ForEach(component => component.Update(gameTime));
         }
 
         private void createInterface()
         {
-            if (Divs.Count == 0)
+            if (divs.Count == 0)
                 return;
 
+            int totalItemHeight = areaSize * divs.Count;
+            int maxBarHeight = Bounds.Height - 15 * 2;
+
+            if (totalItemHeight > maxBarHeight)
+            {
+                components.Add(new Button()
+                {
+                    Bounds = new Rectangle(Bounds.X + Bounds.Width - 15, Bounds.Y + 5, 10, 10),
+                    OnPress = up,
+                    Style = styleButton
+                });
+
+                components.Add(new Button()
+                {
+                    Bounds = new Rectangle(Bounds.X + Bounds.Width - 15, Bounds.Y + Bounds.Height - 15, 10, 10),
+                    OnPress = down,
+                    Style = styleButton
+                });
+
+                Vector2 limit = new Vector2(Bounds.Y + 15 + 10, Bounds.Y + Bounds.Height - 15 - 10);
+                int barHeight = maxBarHeight - (totalItemHeight - maxBarHeight);
+                Rectangle barRectangle = direction == OverflowDirection.Top
+                    ? new Rectangle(Bounds.X + Bounds.Width - 15, Bounds.Y + 15 + 10, 10, barHeight)
+                    : new Rectangle(Bounds.X + Bounds.Width - 15, Bounds.Y + Bounds.Height - barHeight - 15 - 10, 10, barHeight);
+
+                bar = new Bar(
+                    bounds: barRectangle,
+                    style: styleButton,
+                    limit: limit,
+                    wrapIndexer: wrapIndexer
+                );
+
+                components.Add(bar);                    
+            }
 
         }
+
+        private void up()
+        {
+            if (startDivY < Bounds.Y)
+            {
+                wrapIndexer.Indexer += moveSpeed;
+                bar.Bounds.Y -= moveSpeed;
+            }
+        }
+
+        private void down()
+        {
+            if (startDivY + divs.Count * areaSize >= Bounds.Y + Bounds.Height)
+            {
+                wrapIndexer.Indexer -= moveSpeed;
+                bar.Bounds.Y += moveSpeed;
+            }
+        }
+    }
+
+    public class WrapIndexer
+    {
+        public int Indexer;
     }
 }
