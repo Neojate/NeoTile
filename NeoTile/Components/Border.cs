3060bf0e9040dc8fed5216a322b26659b94f3fe6
Albert Alonso <albert15_89@hotmail.com>
2021-11-21 11:44:46 +0100

    Border component

---
+++ NeoTile/Components/Border.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeoTile.Components
+{
+
+    public class Border
+    {
+        private Texture2D texture;
+
+        private Rectangle bounds;
+
+        private Color color;
+
+        private int weight;
+
+        public Border(Texture2D texture, Rectangle bounds, Color color, int weight)
+        {
+            this.texture = texture;
+            this.bounds = bounds;
+            this.color = color;
+            this.weight = weight;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle(bounds.X, bounds.Y, bounds.Width, weight), color);
+            spriteBatch.Draw(texture, new Rectangle(bounds.X, bounds.Y + bounds.Height - weight, bounds.Width, weight), color);
+
+            spriteBatch.Draw(texture, new Rectangle(bounds.X, bounds.Y, weight, bounds.Height), color);
+            spriteBatch.Draw(texture, new Rectangle(bounds.X + bounds.Width - weight, bounds.Y, weight, bounds.Height), color);
+        }
+
+    }
+}
